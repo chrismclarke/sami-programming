7a3414d2cb3ec90cbbcea6568b77116c138c8496
chrismclarke <chris.m.clarke@live.co.uk>
2020-12-26 14:20:26 -0800

    update build scripts

--- scripts/generateAssetList.ts
+++ scripts/generateAssetList.ts
@@ -1,25 +1,31 @@
 import * as fs from "fs-extra";
+import * as yaml from "js-yaml";
 
+/**
+ * Generate a list of all folders that contain assets and write to `pubspec.yaml`
+ */
 function main() {
-  const list = listFoldersRecursively("assets");
-  console.log("list", list);
+  const assetList = listFileFoldersRecursively("assets");
+  const pubspecJson: any = yaml.safeLoad(fs.readFileSync("pubspec.yaml", "utf8"));
+  pubspecJson.flutter.assets = assetList;
+  fs.writeFileSync("pubspec.yaml", yaml.safeDump(pubspecJson));
 }
 main();
 
-function listFoldersRecursively(
-  dir: string,
-  list = { files: [] as any, folders: [] as any }
-) {
-  const files = fs.readdirSync(dir);
-  for (const file of files) {
-    const filepath = `${dir}/${file}`;
-    const stat = fs.statSync(filepath);
-    if (stat.isDirectory()) {
-      list.folders.push(filepath);
-      listFoldersRecursively(filepath, list);
-    } else {
-      list.files.push(file);
+/**
+ *  Recursively list all folders that contain files
+ *  (empty folders will not be listed, but children of empty folders will still be processed)
+ */
+function listFileFoldersRecursively(baseFolder: string, allFolders: string[] = []) {
+  const withFileTypes = true;
+  const folders = fs.readdirSync(baseFolder, { withFileTypes }).filter((f) => f.isDirectory());
+  for (const folder of folders.map((f) => f.name)) {
+    const folderpath = `${baseFolder}/${folder}`;
+    // only record path if it has files inside
+    if (fs.readdirSync(folderpath, { withFileTypes }).filter((f) => f.isFile()).length > 0) {
+      allFolders.push(`${folderpath}/`);
     }
+    listFileFoldersRecursively(folderpath, allFolders);
   }
-  return list;
+  return allFolders;
 }
